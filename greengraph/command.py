75b35575c2c9be8678b85d6564066aefb5a25519
Simon Schaal <nomis05@gmail.com>
2015-12-08 20:27:11 +0000

    Add check for sufficient number of arguments; Add genGraph documentation

--- greengraph/command.py
+++ greengraph/command.py
@@ -1,13 +1,19 @@
 from argparse import ArgumentParser
 from genGraph import plotGraph
+import sys
 
 def process():
-    parser = ArgumentParser(description = "Generate a graph of the proportion of green pixels in a series of satellite images between two points")
+    parser = ArgumentParser(description = "Generates a graph of the proportion of green pixels in a series of satellite images between two points")
     parser.add_argument('--from', dest="start", help="Starting city for Greengraph")
     parser.add_argument('--to', dest="end", help="End city for Greengraph")
     parser.add_argument('--steps', help="Number of images to process between the cities")
     parser.add_argument('--out', help="Destination file to save the graph")
     arguments= parser.parse_args()
+
+    #Make sure all necessary arguments are given
+    if len(sys.argv[1:])<3:
+        parser.print_help()
+        parser.exit()
 
     plotGraph(arguments.start, arguments.end, arguments.steps, arguments.out)
  
--- greengraph/genGraph.py
+++ greengraph/genGraph.py
@@ -1,12 +1,29 @@
 from matplotlib import pyplot as plt
 from graph import Greengraph
 
-def plotGraph(start, end, steps, out):
+def plotGraph(start, end, steps, out=None):
+    """ Generate a greeting string for a person.
+    Parameters
+    ----------
+    start: str
+    Starting city.
+    end: str
+    Ending city.
+    steps: int
+    Gives number of images to process between start and end.
+    out: str
+    Specifies output file for graph. If not given then graph will be displayed. 
+    Returns
+    -------
+    Nothing
+    """
+
     mygraph=Greengraph(start, end)
     data = mygraph.green_between(steps)
     plt.plot(data)
 
     if out:
         plt.savefig(out)
-    plt.show()
+    else:
+        plt.show()
 
